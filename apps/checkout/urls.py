0b296c09b861c6bd2f2409d9991bed8489fce0f7
Darius-Ndubi <ndubidarius@gmail.com>
2019-10-24 22:05:34 +0300

    purchase invoice

--- apps/checkout/urls.py
+++ apps/checkout/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path
 
 # Local imports
-from .views import PostUserCheckoutView
+from .views import (
+    PostUserCheckoutView,
+    UserPurchaseInvoiceView
+)
 
 urlpatterns = [
     path('', PostUserCheckoutView.as_view(), name="post_user_checkout"),
+    path('purchase', UserPurchaseInvoiceView.as_view(), name="user_purchase")
 ]
--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -3,12 +3,19 @@
 # restframe work imports
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
+
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 # Local imports
-from .serializers import AddCheckoutSerializer
-from ..authentication.backends import authenticate_credentials
+from .serializers import (
+    AddCheckoutSerializer,
+    PurchaseSerializer
+)
+from .checkout_utility import (
+    append_user_id,
+    FortnoxInvoice
+)
 
 
 """
@@ -32,19 +39,16 @@ class PostUserCheckoutView(CreateAPIView):
     serializer_class = AddCheckoutSerializer
 
     def post(self, request):
-
-        # Validate token in payload
-        user = authenticate_credentials(request.data['api_token'])
-        # Pass user id as part of the data
-        request.data['user_id'] = user.id
+        # append user id to request 
+        request = append_user_id(request)
 
         serializer = self.serializer_class(
-            data=request.data,
+            data=request['request'].data,
             context={'request': request}
         )
 
         # Validate data entered
-        AddCheckoutSerializer.validate_card(data=request.data)
+        AddCheckoutSerializer.validate_card(data=request['request'].data)
 
         if serializer.is_valid():
             serializer.save()
@@ -56,3 +60,49 @@ class PostUserCheckoutView(CreateAPIView):
             return Response(response_message, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserPurchaseInvoiceView(CreateAPIView):
+    permission_classes = (AllowAny, )
+    serializer_class = PurchaseSerializer
+
+    def post(self, request):
+        # append user id to request
+        request = append_user_id(request)
+
+        # process data with fortnox
+        fortnox_response = FortnoxInvoice().post_invoice_to_fortnox(request)
+        invoice_data = eval(fortnox_response['message']['body'])
+
+        # import pdb; pdb.set_trace()
+        if fortnox_response['message']['status'] == '201':
+            # set invoice number to the one sent to fortnox
+            request['request'].data['invoice_number'] = fortnox_response['invoice_number']
+            request['request'].data['fortnox_invoice_url'] = invoice_data['Invoice']['@url']
+            request['request'].data['fortnox_customer_id'] = invoice_data['Invoice']['CustomerNumber']
+            request['request'].data['fortnox_response_body'] = str(invoice_data['Invoice'])
+
+            serializer = self.serializer_class(
+                data=request['request'].data
+            )
+
+            # Save checkout data
+            if serializer.is_valid():
+                serializer.save()
+
+                response_message = {
+                    "message": "Purchase is being processed",
+                    "data": serializer.data,
+                    "fortnox_response": fortnox_response
+                }
+
+                return Response(response_message, status=status.HTTP_201_CREATED)
+            # Raise validation error if data is invalid
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Return any error that was not caught
+        return Response({
+            "message": "Sorry, It's not you its us",
+            "status": fortnox_response['message']['status'],
+            "data": fortnox_response['message']
+        })
